ba9b69c09ca9c12b79cd3dbc19ccd44226a6e21c
[Solomon NjobNjobvu] <[Solomonnjobvu1@gmail.com]>
2020-09-16 02:51:49 +0200

    Returns data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {fetchWeather} from './api/FetchWeather'
+import './App.css'
 
 function App() {
+    const [query, setQuery] = useState('')
+
+    const handleChange = event => {
+        setQuery(event.target.value)
+    }
+
+    const searchQuery = async event => {
+        if(event.key === "Enter"){
+            const data = await fetchWeather(query)
+
+            console.log(data)
+        }
+    }
     return (
-        <div>
-            <h1>Welcome, Solomon</h1>
+        <div className='weather-app-container'>
+            <input
+            type='text'
+            className ='input-query'
+            placeholder = 'Search City...'
+            value = {query}
+            onChange = {handleChange}
+            onKeyPress = {searchQuery}
+            />
         </div>
     )
 }
